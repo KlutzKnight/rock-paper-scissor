cfa099b70753354fd7ef470080c9681d05529bc8
Rafi_Xubuntu <215167856+Rafi-Ahmad1@users.noreply.github.com>
2026-01-24 13:43:28 +0530

    feat: add playRound
    Plays a single round of Rock-Paper-Scissor

--- janken.js
+++ janken.js
@@ -1,3 +1,8 @@
+// Global variables for Score  Keeping
+let computerScore = 0;
+let humanScore = 0;
+
+//Randomly generates computer's choice
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if(computerChoice == 0)
@@ -8,10 +13,53 @@ function getComputerChoice() {
         return "scissor";
 }
 
+//Asks the user for their choice
 function getHumanChoice() {
     let humanChoice = prompt("Enter your Choice: ");
     return humanChoice.toLowerCase();
 }
 
-let computerScore = 0;
-let humanScore = 0;
+//Plays a single round of rock-paper-scissor
+function playRound(computerChoice ,humanChoice) {
+    //Both chose the same
+    if(computerChoice == humanChoice) {
+        console.log(`It's a Tie! Both chose ${computerChoice}`)
+        computerScore++;
+        humanScore++;
+    }
+    //Compuer chose "Rock"
+    else if(computerChoice == "rock") {
+        if(humanChoice == "paper") {
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+            humanScore++;
+        } else {
+            console.log(`You Lose :( ${computerChoice} beats ${humanChoice}`)
+            computerScore++;
+        }
+    }
+    //Computer chose "Paper"
+    else if (computerChoice == "paper") {
+        if(humanChoice == "scissor") {
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+            humanScore++;
+        } else {
+            console.log(`You Lose :( ${computerChoice} beats ${humanChoice}`)
+            computerScore++;
+        }
+    }
+    //Computer chose "Scissor"
+    else {
+        if(humanChoice == "rock") {
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+            humanScore++;
+        } else {
+            console.log(`You Lose :( ${computerChoice} beats ${humanChoice}`)
+            computerScore++;
+        }
+    }
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(computerSelection, humanSelection);
